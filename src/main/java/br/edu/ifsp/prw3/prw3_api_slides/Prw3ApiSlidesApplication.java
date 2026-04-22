56e0eb3b7b0155cc5ee52518996c7847b960eabd
FLCL84L <filipe.etecera@gmail.com>
2026-04-22 20:14:58 -0300

    first commit

---
+++ src/main/java/br/edu/ifsp/prw3/prw3_api_slides/Prw3ApiSlidesApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.prw3.prw3_api_slides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prw3ApiSlidesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prw3ApiSlidesApplication.class, args);
+	}
+
+}
